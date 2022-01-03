d9cb77b173984a526ed295af8dcc0e8b63931ca8
vanja032 <sretenovic.vanja32@gmail.com>
2022-01-03 01:19:44 +0100

    First Commit 01:18h 03/01/2022 DataBase Tables - Proizvod, Korisnik

---
+++ SI2021-CoffeeMachine/SI2021-CoffeeMachine/dbo.Proizvod.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Proizvod]
+(
+	[ID_Proizvoda] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Naziv] NVARCHAR(50) NOT NULL, 
+    [Opis] NVARCHAR(100) NOT NULL, 
+    [FK_ID_Proizvodjaca] INT NOT NULL
+)
